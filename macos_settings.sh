ee4bb98deed92286e94613fbd51af64ecce6756e
semklauke <privat@semklauke.de>
2024-12-20 01:26:23 +0100

    some macos settings

---
+++ macos_settings.sh
@@ -0,0 +1,9 @@
+####################################################################
+# Macos Applications Settings                                      #
+####################################################################
+# Skim autoreload
+defaults write -app Skim SKAutoReloadFileUpdate -boolean true
+# umlaut press and hold
+defaults write -g ApplePressAndHoldEnabled -bool false
+#
+defaults write com.microsoft.VSCode SecureKeyboardEntry -bool false
